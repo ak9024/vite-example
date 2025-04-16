db7bb4d8004bb25ef130f96d0d06116faaac9a69
Adiatma Kamarudin <adiatma.mail@gmail.com>
2025-04-16 11:50:54 +0700

    chore: add comments

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { router } from './router'
 import './index.css'
 
+// Create a new Vue application instance using the main App component
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
