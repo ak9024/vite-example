64b543a3568512d86efdd2aeda170334ebd69f7e
Adiatma Kamarudin <adiatma.mail@gmail.com>
2025-04-17 21:19:15 +0700

    chore: add whitespace

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex justify-between pl-3 mb-4">
     <span>{{ name }}</span>
+    
     <button
       class="border rounded-md py-1 px-3 text-sm text-gray-700 select-none"
       @click="$emit('confirm')"
